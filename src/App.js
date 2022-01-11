837aa2a60d11ad99933efd7096547515948daf9e
Benorr91 <95618872+Benorr91@users.noreply.github.com>
2022-01-11 14:11:04 +0200

    aa

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import React from 'react';
 import './App.css';
-import AppConvert from './comp/appConvert';
+
 
 function App() {
 
 
   return (
     <>
-    <AppConvert/>
+   
     </>
   );
 }
